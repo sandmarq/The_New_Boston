9a4f03e04494e10355b2e1df51be0e65ba69552c
Sandrine Marquis <sandrine.marquis30@gmail.com>
2014-12-02 20:46:56 -0500

    After tuto the new boston 27

--- app/src/main/java/thenewboston/sandmarq/ca/thenewboston/TextPlay.java
+++ app/src/main/java/thenewboston/sandmarq/ca/thenewboston/TextPlay.java
@@ -2,6 +2,7 @@ package thenewboston.sandmarq.ca.thenewboston;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,16 +21,16 @@ public class TextPlay extends Activity {
 
         Button chkCmd = (Button) findViewById(R.id.bResults);
         final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
-        EditText input = (EditText) findViewById(R.id.etCommands);
+        final EditText input = (EditText) findViewById(R.id.etCommands);
         TextView display = (TextView) findViewById(R.id.tvResults);
 
         passTog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (passTog.isChecked()){
-
+                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 } else {
-
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
                 }
             }
         });
