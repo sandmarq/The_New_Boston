39d0f058d23552d03d209188dfba7460143ba5ff
Sandrine Marquis <sandrine.marquis30@gmail.com>
2014-12-02 20:28:35 -0500

    After tuto the new boston 25

---
+++ app/src/main/java/thenewboston/sandmarq/ca/thenewboston/TextPlay.java
@@ -0,0 +1,26 @@
+package thenewboston.sandmarq.ca.thenewboston;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+/**
+ * Created by sandrine on 2014-12-02.
+ */
+public class TextPlay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text);
+
+        Button chkCmd = (Button) findViewById(R.id.bResults);
+        ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+        EditText input = (EditText) findViewById(R.id.etCommands);
+        TextView display = (TextView) findViewById(R.id.tvResults);
+
+    }
+}
