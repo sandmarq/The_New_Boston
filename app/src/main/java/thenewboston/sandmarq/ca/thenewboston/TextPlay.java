4b5420cfb1da29209929f53ab94987bcdde0dae0
Sandrine Marquis <sandrine.marquis30@gmail.com>
2014-12-03 19:39:36 -0500

    After tuto the new boston 29

--- app/src/main/java/thenewboston/sandmarq/ca/thenewboston/TextPlay.java
+++ app/src/main/java/thenewboston/sandmarq/ca/thenewboston/TextPlay.java
@@ -3,6 +3,7 @@ package thenewboston.sandmarq.ca.thenewboston;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,12 +23,12 @@ public class TextPlay extends Activity {
         Button chkCmd = (Button) findViewById(R.id.bResults);
         final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
         final EditText input = (EditText) findViewById(R.id.etCommands);
-        TextView display = (TextView) findViewById(R.id.tvResults);
+        final TextView display = (TextView) findViewById(R.id.tvResults);
 
         passTog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (passTog.isChecked()){
+                if (passTog.isChecked()) {
                     input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 } else {
                     input.setInputType(InputType.TYPE_CLASS_TEXT);
@@ -40,10 +41,12 @@ public class TextPlay extends Activity {
             public void onClick(View v) {
                 String check = input.getText().toString();
                 if (check.contentEquals("left")) {
-
+                    display.setGravity(Gravity.LEFT);
                 } else if (check.contentEquals("center")) {
-
+                    display.setGravity(Gravity.CENTER);
                 } else if (check.contentEquals("right")) {
+                    display.setGravity(Gravity.RIGHT);
+                } else if (check.contentEquals("blue")) {
 
                 }
             }
